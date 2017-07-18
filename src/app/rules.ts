58a6d2cb874473215aaa6f91ee2610102433bda4
Vladwoguer Bezerra <vladwoguerbezerra@gmail.com>
2017-07-17 22:29:43 -0300

    Task 7 finished

--- src/app/rules.ts
+++ src/app/rules.ts
@@ -5,12 +5,16 @@ import {
     EntityDetailsComponent, ShowLineComponent,
     ListingTableComponent
 } from 'angularm';
+import { ShowTagComponent } from "./widgets/show.tag";
 
 export let defineRules = (angularm: AngularmService) => {
     angularm
         .ptr('form_line', '*', 'code', null, FormLineComponent, { inputType: 'number' })
         .dptr('form_line', FormLineComponent, { inputType: 'text' })
         .dpr('show_line', ShowLineComponent)
+        .pr('show_line','*','tags',null,ShowTagComponent, {tagClass:'w3-tag w3-green'})
+        .pr('show_line','*','labels',null,ShowTagComponent, {tagClass:'w3-tag w3-red'})
+        .pr('show_line','*','keywords',null,ShowTagComponent, {tagClass:'w3-tag w3-blue'})
         .detr('list_entities', ListingTableComponent)
         .der('table_line', EntityLineComponent)
         .der('show_entity', EntityDetailsComponent)
